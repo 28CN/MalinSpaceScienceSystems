5018984de491c43101fd2394ecf2956f9bb208b1
Naiguang <30076585@tafe.wa.edu.au>
2025-10-24 10:23:43 +0800

    All changes after signoff

---
+++ AstroServer.WebApi/Models/ApiModels.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AstroServer.WebApi.Models
+{
+    // VelocityDtos
+    public sealed class VelocityRequest
+    {
+        [Range(double.Epsilon, double.MaxValue)]
+        public double ObservedWavelength { get; set; }
+        [Range(double.Epsilon, double.MaxValue)]
+        public double RestWavelength { get; set; }
+    }
+    public sealed class VelocityResponse
+    {
+        public double VelocityMps { get; set; }
+    }
+
+    // DistanceDtos
+    public sealed class DistanceRequest
+    {
+        [Range(double.Epsilon, double.MaxValue)]
+        public double ParallaxArcseconds { get; set; }
+    }
+    public sealed class DistanceResponse
+    {
+        public double DistanceParsec { get; set; }
+    }
+
+    // KelvinDtos.cs
+    public sealed class KelvinRequest
+    {
+        [Range(-273.15, double.MaxValue)]
+        public double Celsius { get; set; }
+    }
+    public sealed class KelvinResponse
+    {
+        public double Kelvin { get; set; }
+    }
+
+    // EventHorizonDtos.cs
+    public sealed class EventHorizonRequest
+    {
+        [Range(double.Epsilon, double.MaxValue)]
+        public double MassKg { get; set; }
+    }
+    public sealed class EventHorizonResponse
+    {
+        public double RadiusMeters { get; set; }
+    }
+}
